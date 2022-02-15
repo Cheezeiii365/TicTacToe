dd517ac46bfcd43f726dc4b81ae2c1300bfa5542
BernieOdoy004 <cheezeiii365@gmail.com>
2022-02-15 09:42:47 -0500

    start pygame gui

---
+++ GUIBuild.py
@@ -0,0 +1,87 @@
+# Import the pygame module
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+
+# Constants for H/W
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((0, 0, 0))
+        self.rect = self.surf.get_rect()
+
+    # Move the sprite based on user keypresses
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+# Initialize pygame
+pygame.init()
+
+# myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+# textsurface = myfont.render('Some Text', False, (0, 0, 0))
+
+
+# Create screen object
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+# Instantiate player
+player = Player()
+
+# main
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    # fill BG w/ white
+    screen.fill((255, 255, 255))
+
+    # draw player
+    screen.blit(player.surf, player.rect)
+    
+    # screen.blit(textsurface, (0, 0))
+    
+    # update display
+    pygame.display.flip()
+    
