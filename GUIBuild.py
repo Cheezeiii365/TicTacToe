ae224cb522bd1e5d4d34110cff2b97a18f8952c6
BernieOdoy004 <cheezeiii365@gmail.com>
2022-02-15 11:03:44 -0500

    Update GUIBuild.py

--- GUIBuild.py
+++ GUIBuild.py
@@ -18,36 +18,27 @@ from pygame.locals import (
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-class Player(pygame.sprite.Sprite):
+class Grid(pygame.sprite.Sprite):
     def __init__(self):
-        super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        super(Grid, self).__init__()
+        self.surf = pygame.Surface((400, 10))
         self.surf.fill((0, 0, 0))
         self.rect = self.surf.get_rect()
+        self.rect.move_ip(-30, 30)
 
-    # Move the sprite based on user keypresses
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
 
-        # Keep player on the screen
-        if self.rect.left < 0:
-            self.rect.left = 0
-        if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
-        if self.rect.top <= 0:
-            self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
 
 # Initialize pygame
 pygame.init()
+
+line_color = (0, 0, 0)
+def draw():
+    pygame.draw.line(screen, line_color, (SCREEN_WIDTH / 3, 0), (SCREEN_WIDTH / 3, SCREEN_HEIGHT), 7)
+    pygame.draw.line(screen, line_color, (SCREEN_WIDTH / 3 * 2, 0), (SCREEN_WIDTH / 3 * 2, SCREEN_HEIGHT), 7)
+
+    # drawing horizontal lines
+    pygame.draw.line(screen, line_color, (0, SCREEN_HEIGHT / 3), (SCREEN_WIDTH, SCREEN_HEIGHT / 3), 7)
+    pygame.draw.line(screen, line_color, (0, SCREEN_HEIGHT / 3 * 2), (SCREEN_WIDTH, SCREEN_HEIGHT / 3 * 2), 7)
 
 # myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
@@ -58,11 +49,11 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # Instantiate player
-player = Player()
+# grid = Grid()
 
 # main
 running = True
-
+draw()
 while running:
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -71,14 +62,12 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys)
 
     # fill BG w/ white
     screen.fill((255, 255, 255))
 
-    # draw player
-    screen.blit(player.surf, player.rect)
+    # draw grid
+    screen.blit(grid.surf, grid.rect)
     
     # screen.blit(textsurface, (0, 0))
     
